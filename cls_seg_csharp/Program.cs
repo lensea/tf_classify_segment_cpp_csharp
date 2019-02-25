bc9de46287944e93dbac6ba5caa6f7f04a51717c
lensea <lensea@users.noreply.github.com>
2019-02-25 16:56:42 +0800

    Add files via upload

--- cls_seg_csharp/Program.cs
+++ cls_seg_csharp/Program.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using manage_dll;
+using ManageDll;
 
 
-namespace cls_seg_csharp
+namespace ClsSegCsharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string s = "E:/Data/solder/test/neg/324.jpg";
-            manage_cls cls_info = new manage_cls(s);
+            string str = "E:/Data/solder/test/neg/324.jpg";
+            ManageCls cls_info = new ManageCls(str);
             Console.WriteLine("class_id:{0},prob:{1}", cls_info.cls_id, cls_info.cls_prob);
             Console.ReadLine();
-            string s1= "E:/Data/solder_seg/test/1_000010.jpg";
-            //manage_seg seg_info = new manage_seg();
-            manage_seg seg_info = new manage_seg(s1);
-            int[] info = seg_info.get_reult();
+            string str1= "E:/Data/solder_seg/test/1_000010.jpg";
+            //ManageSeg seg_info = new ManageSeg();
+            ManageSeg seg_info = new ManageSeg(str1);
+            int[] info = seg_info.GetReult();
             for(int i=0;i<65536;++i)
             {
                 Console.WriteLine("seg_info[{0}]:{1}",i,info[i]);
